35b131f7635f9d1ec9aed17ac83f610b33eefe1e
jzh <1002530660@qq.com>
2023-11-22 17:33:37 +0800

    提取公共方法，以及基础版IOC的测试

--- src/com/minis/context/ClassPathXmlApplicationContext.java
+++ src/com/minis/context/ClassPathXmlApplicationContext.java
@@ -16,6 +16,22 @@ public class ClassPathXmlApplicationContext{
     Map<String, Object> singletonMap = new HashMap<>();
     List<BeanDefinition> beanDefinitions = new ArrayList<BeanDefinition>();
     public ClassPathXmlApplicationContext(String fileName){
+        this.readXml(fileName);
+        this.instanceBeans();
+    }
+
+    private void instanceBeans() {
+        //通过反射初始化对象
+        for (BeanDefinition beanDefinition : beanDefinitions) {
+            try {
+                singletonMap.put(beanDefinition.getId(), Class.forName(beanDefinition.getClassName()).newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void readXml(String fileName) {
         SAXReader reader = new SAXReader();
         try {
             URL url = this.getClass().getClassLoader().getResource(fileName);
@@ -31,18 +47,9 @@ public class ClassPathXmlApplicationContext{
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        //通过反射初始化对象
-        for (BeanDefinition beanDefinition : beanDefinitions) {
-            try {
-                singletonMap.put(beanDefinition.getId(), Class.forName(beanDefinition.getClassName()).newInstance());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 
-    public Object getBean(String beanName) throws Exception {
+    public Object getBean(String beanName){
         //TODO 为什么这个方法不判断Bean是否存在
 //        if(!beanNames.contains(beanName)) {
 //            throw new Exception("No bean found");
--- src/com/minis/test/Test1.java
+++ src/com/minis/test/Test1.java
@@ -4,7 +4,11 @@ import com.minis.context.ClassPathXmlApplicationContext;
 
 public class Test1 {
 
+
 	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		AService service = (AServiceImpl)context.getBean("aservice");
+		service.sayHello();
 	}
 
 }
