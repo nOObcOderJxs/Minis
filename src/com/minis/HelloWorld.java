dbb12b1aaaf7728739b7b5caeccc59deaabc49a8
jzh <1002530660@qq.com>
2023-11-22 15:57:36 +0800

    初版xmlContext构建

--- src/com/minis/HelloWorld.java
+++ src/com/minis/HelloWorld.java
@@ -1,7 +1,24 @@
 package com.minis;
 
+import com.minis.context.ClassPathXmlApplicationContext;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
+import java.net.URL;
+
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+//        SAXReader reader = new SAXReader();
+//        try {
+//            URL url = "";
+//            Document document = reader.read("beans.xml");
+//        } catch (DocumentException e) {
+//            throw new RuntimeException(e);
+//        }
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(Class.forName("com.minis.context.ClassPathXmlApplicationContext").getClassLoader().getResource(""));
         System.out.println("Hello World!");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
     }
 }
--- src/com/minis/context/ClassPathXmlApplicationContext.java
+++ src/com/minis/context/ClassPathXmlApplicationContext.java
@@ -1,6 +1,12 @@
 package com.minis.context;
 
 
+import com.minis.beans.BeanDefinition;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,15 +14,39 @@ import java.util.Map;
 
 public class ClassPathXmlApplicationContext{
     Map<String, Object> singletonMap = new HashMap<>();
-    List<String> beanNames = new ArrayList<>();
-    public ClassPathXmlApplicationContext(String fileName) {
+    List<BeanDefinition> beanDefinitions = new ArrayList<BeanDefinition>();
+    public ClassPathXmlApplicationContext(String fileName){
+        SAXReader reader = new SAXReader();
+        try {
+            URL url = this.getClass().getClassLoader().getResource(fileName);
+            Document document = reader.read(url);
+            Element rootElement = document.getRootElement();
+            for (Element element : (List<Element>)rootElement.elements()) {
+                //强制转化(List<Element>) 向下转换，如果不是这样的话会报ClassCastException
+                String beanID = element.attributeValue("id");
+                String beanClassName = element.attributeValue("class");
+                BeanDefinition beanDefinition = new BeanDefinition(beanID, beanClassName);
+                beanDefinitions.add(beanDefinition);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
+        //通过反射初始化对象
+        for (BeanDefinition beanDefinition : beanDefinitions) {
+            try {
+                singletonMap.put(beanDefinition.getId(), Class.forName(beanDefinition.getClassName()).newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public Object getBean(String beanName) throws Exception {
-        if(!beanNames.contains(beanName)) {
-            throw new Exception("No bean found");
-        }
+        //TODO 为什么这个方法不判断Bean是否存在
+//        if(!beanNames.contains(beanName)) {
+//            throw new Exception("No bean found");
+//        }
         return singletonMap.get(beanName);
     }
 }
